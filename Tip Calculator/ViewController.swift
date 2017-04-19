66a7b8527c0ae5bd7e31f4bbfa61e7633fcdb251
Megan Wiley <mwiley322@gmail.com>
2017-04-19 16:37:56 -0700

    initial commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let image : UIImage = UIImage(named: "roz")!
+        self.myImageView.image = image
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("hi I am appearing")
     }
+    
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var myImageView: UIImageView!
+    
 
+    @IBAction func calculateTip(_ sender: Any) {
+        let billAmountString = billAmountTextField.text ?? "default val"
+        let billAmount = Double(billAmountString) ?? 0
+        let tipAmount = billAmount * 0.15
+        let totalAmount = tipAmount + billAmount
+        tipAmountLabel.text = "$\(tipAmount)"
+        totalAmountLabel.text = "$\(totalAmount)"
+        view.endEditing(true)
+    }
 
 }
 
